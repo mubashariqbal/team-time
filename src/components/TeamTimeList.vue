de0196aaec1248373826a735f3e7a266e9e8b4ce
Mubashar Iqbal <me@mubs.me>
2016-09-18 19:44:17 -0400

    test geo on ssl

---
+++ src/components/TeamTimeList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="teamtime-list">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    console.log('abc')
+    if ('geolocation' in navigator) {
+      console.log('yes geolocation')
+      navigator.geolocation.getCurrentPosition(function (position) {
+        console.log(position.coords.latitude, position.coords.longitude)
+      })
+    } else {
+      console.log('oh-no, no geolocation')
+    }
+    return {
+      msg: 'team-time-list'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
